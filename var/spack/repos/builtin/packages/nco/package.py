fcc586efd87b7780ef7adeba3ec8570a971bb992
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-01-10 21:49:03 +0100

    Update to NCO 5.1.4 release

--- var/spack/repos/builtin/packages/nco/package.py
+++ var/spack/repos/builtin/packages/nco/package.py
@@ -13,7 +13,7 @@ class Nco(AutotoolsPackage):
     homepage = "http://nco.sourceforge.net/"
     url = "https://github.com/nco/nco/archive/5.0.1.tar.gz"
 
-    version("5.1.4-alpha06", sha256="b4a5fc24e63712e14fbc6beafe6ebde31704fbfdf8ac4b8605af540ed71f4767")
+    version("5.1.4", sha256="4b1ec67b795b985990620be7b7422ecae6da77f5ec93e4407b799f0220dffc88")
     version("5.1.3", sha256="f6766627dab3f067c88f2cd713e3058c324ea4f900fabf9755bdd8918c32de7b")
     version("5.1.2", sha256="1b86303fc55b5a52b52923285a5e709de82cbc1630e68b64dce434b681e4100a")
     version("5.1.1", sha256="6e004cfaa8e3188f543d2a31f7fdd7b2d2a59b6c7fea44c41ad13232bbe1c8d6")
