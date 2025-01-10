cdbe8380c621c55c9717c9a6b5305a2407056d9d
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-01-10 13:55:17 +0100

    Add scorpio 1.6.6 and 1.6.7

--- var/spack/repos/builtin/packages/scorpio/package.py
+++ var/spack/repos/builtin/packages/scorpio/package.py
@@ -15,6 +15,8 @@ class Scorpio(CMakePackage):
     url      = "https://github.com/E3SM-Project/scorpio/archive/refs/tags/scorpio-v1.2.2.tar.gz"
 
     maintainers("xylar", "altheaden")
+    version("1.6.7", sha256="ae7b0eb9d1d48331f3103a9b226855ad46916152ff569f25c6f82b17211008f3")
+    version("1.6.6", sha256="19672bbe987e57c548161f11f0b7c267943969f2e60a9639181d3a040aedff53")
     version("1.6.5", sha256="b81af12f3cadec25898c9b102775f77457f3dd95b878ee6c7edaf6eb0b16d4fc")
     version("1.6.4", sha256="4739718c82f39fa8f9922280175dcdab56786193df8f6fb07f145df1274ed828")
     version("1.6.3", sha256="66350046fa22c8a06fffb8bf2a0fc48c66f05bf00da3a6ab83a0fad9c3c91da4")
