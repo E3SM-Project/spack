cc757747a7ce47a02058ffd0d0adcbc0179b823d
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-07-28 11:44:09 +0200

    Add e3sm-scorpio v2.0.3

--- var/spack/repos/builtin/packages/e3sm-scorpio/package.py
+++ var/spack/repos/builtin/packages/e3sm-scorpio/package.py
@@ -15,6 +15,7 @@ class E3smScorpio(CMakePackage):
     url = "https://github.com/E3SM-Project/scorpio/archive/refs/tags/scorpio-v1.4.1.tar.gz"
 
     maintainers("xylar", "altheaden")
+    version("2.0.3", sha256="ff9570e250e6b75b723b778b17453d9bcc88d2a66674f1bcb0aaab65f321ff60")
     version("2.0.2", sha256="68444fd641363388d0d06528ce7d9d0b33df3ae6103df122973d9963476e3319")
     version("2.0.1", sha256="0a4006fd6f2ce03acbe0f69726cedb32bc1e0dfb5e170e9f8d7201c1125dbc99")
     version("2.0.0", sha256="d82d0c5db34b4d83de0d40130340ad286432b00764d27f4694626e44c525ea80")
